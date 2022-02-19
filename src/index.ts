089f10df0cc660ca34ed88df68f0a26676808fa7
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2022-02-19 19:35:01 +0500

    map integrated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
-const user = new User();
-console.log("user", user);
+// const user = new User();
+// console.log("user", user);
 
-const company = new Company();
-console.log("Company", company);
+// const company = new Company();
+// console.log("Company", company);
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
